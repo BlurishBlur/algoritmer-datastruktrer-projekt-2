7981fc5f6a0ea50a433c612d5ba92aceade8609a
Niels <Niels@DESKTOP-4QGMF1S>
2017-03-22 13:11:39 +0100

    kommentjarer

--- AlgoritmerDatastrukturerProjekt2/src/DictBinTree.java
+++ AlgoritmerDatastrukturerProjekt2/src/DictBinTree.java
@@ -4,14 +4,31 @@
  */
 public class DictBinTree implements Dict {
     
+    /**
+     * The root of the tree.
+     */
     private Node root;
+    /**
+     * The total amount of elements in the tree.
+     */
     private int size;
+    /**
+     * The pointer used for doing an ordered traversal.
+     */
     private int pointer;
     
+    /**
+     * Instantiates the tree, with a size of 0.
+     */
     public DictBinTree() {
         size = 0;
     }
 
+    /**
+     * Inserts a key into the tree, first by comparing the key to the 
+     * other nodes in the tree, starting from the root.
+     * @param key The key wished to be inserted into the tree.
+     */
     @Override
     public void insert(int key) {
         Node y = null;
@@ -38,6 +55,11 @@ public class DictBinTree implements Dict {
         size++;
     }
 
+    /**
+     * Traverses through the tree, returning an integer array with all
+     * elements in order.
+     * @return all elements in order.
+     */
     @Override
     public int[] orderedTraversal() {
         int[] array = new int[size];
@@ -45,7 +67,13 @@ public class DictBinTree implements Dict {
         inorderTreeWalk(root, array);
         return array;
     }
-    
+
+    /**
+     * Method to recursively traverse through the tree, and insert the
+     * keys, in order, into the array.
+     * @param x the starting node, from which to traverse.
+     * @param array the array in which to insert the result.
+     */
     private void inorderTreeWalk(Node x, int[] array) {
         if(x != null) {
             inorderTreeWalk(x.getLeftChild(), array);
@@ -54,6 +82,16 @@ public class DictBinTree implements Dict {
         }
     }
 
+    /**
+     * Searches for a given key, and returns whether or not the key is 
+     * contained in the tree. Starting at the root, it compares the key
+     * to that node. If the key is smaller, it continues down the left
+     * part of the tree, and if it's larger, it continues down the right
+     * part of the tree.
+     * @param key the key to be checked whether is contained in the tree
+     * or not.
+     * @return 
+     */
     @Override
     public boolean search(int key) {
         Node x = root;
