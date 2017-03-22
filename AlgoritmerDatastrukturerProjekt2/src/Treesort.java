50a2959bf5e702c091a3ee7851a13972370a2cb0
Niels <Niels@DESKTOP-4QGMF1S>
2017-03-22 13:03:55 +0100

    færdig med del 2

---
+++ AlgoritmerDatastrukturerProjekt2/src/Treesort.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Niels Heltner (nhelt15) & Antonio Lascari (anlas15)
+ */
+public class Treesort {
+    
+    public static void main(String[] args) {
+        Dict dict = new DictBinTree();
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextInt()) {
+            int key = in.nextInt();
+            dict.insert(key);
+        }
+        for(int i : dict.orderedTraversal()) {
+            System.out.println(i);
+        }
+    }
+    
+}
